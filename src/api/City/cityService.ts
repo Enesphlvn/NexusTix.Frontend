f4a0155da6e4de6a9570d7de21a75c04d0f287c0
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-10 15:58:59 +0300

    Added cityService

---
+++ src/api/City/cityService.ts
@@ -0,0 +1,33 @@
+import type { City } from "../../models/City/City";
+import type { ServiceResult } from "../../models/ServiceResult";
+import api from "../api";
+
+export const getAllCities = async (): Promise<City[]> => {
+  try {
+    const response = await api.get<ServiceResult<City[]>>("/cities");
+
+    if (response.data.isSuccess) {
+      return response.data.data;
+    } else {
+      throw new Error(response.data.errorMessages.join(", "));
+    }
+  } catch (error) {
+    console.error("Şehirler getirilirken hata oluştu:", error);
+    throw error;
+  }
+};
+
+export const getCity = async (id: number): Promise<City> => {
+  try {
+    const response = await api.get<ServiceResult<City>>(`/cities/${id}`);
+
+    if (response.data.isSuccess) {
+      return response.data.data;
+    } else {
+      throw new Error(response.data.errorMessages.join(", "));
+    }
+  } catch (error) {
+    console.error(`Şehir (ID: ${id}) getirilirken hata oluştu:`, error);
+    throw error;
+  }
+};
--- src/api/api.ts
+++ src/api/api.ts
@@ -6,4 +6,13 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("nexusTixToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
