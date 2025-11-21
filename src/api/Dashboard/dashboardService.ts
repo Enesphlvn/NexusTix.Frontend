0ec86d28263fa1c19bdea17e84d24402aece4368
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-21 13:38:47 +0300

    Added getDashboardCharts to dashboardService

--- src/api/Dashboard/dashboardService.ts
+++ src/api/Dashboard/dashboardService.ts
@@ -1,3 +1,4 @@
+import type { DashboardChartResponse } from "../../models/Dashboard/Responses/DashboardChartResponse";
 import type { DashboardSummaryResponse } from "../../models/Dashboard/Responses/DashboardSummaryResponse";
 import type { ServiceResult } from "../../models/ServiceResult";
 import api from "../api";
@@ -6,6 +7,14 @@ export const getDashBoardSummary = async (): Promise<DashboardSummaryResponse> =
     const response = await api.get<ServiceResult<DashboardSummaryResponse>>('/dashboards/summary');
 
     if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", ")); 
+
+    return response.data.data;
+}
+
+export const getDashboardCharts = async (): Promise<DashboardChartResponse> => {
+    const response = await api.get<ServiceResult<DashboardChartResponse>>('/dashboards/charts');
+
+    if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
 
     return response.data.data;
 }
