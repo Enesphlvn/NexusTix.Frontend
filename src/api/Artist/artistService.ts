9ce6bf99206d10db86a18c2658a20f5da897fa17
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-28 13:41:05 +0300

    Added artistService

---
+++ src/api/Artist/artistService.ts
@@ -0,0 +1,62 @@
+import type { CreateArtistRequest } from "../../models/Artist/Requests/CreateArtistRequest";
+import type { UpdateArtistRequest } from "../../models/Artist/Requests/UpdateArtistRequest";
+import type { ArtistResponse } from "../../models/Artist/Responses/ArtistResponse";
+import type { ArtistWithEventsResponse } from "../../models/Artist/Responses/ArtistWithEventsResponse";
+import type { ServiceResult } from "../../models/ServiceResult";
+import api from "../api";
+
+export const getAllArtists = async (): Promise<ArtistResponse[]> => {
+  const response = await api.get<ServiceResult<ArtistResponse[]>>("/artists");
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+
+  return response.data.data!;
+};
+
+export const getArtist = async (id: number): Promise<ArtistResponse> => {
+  const response = await api.get<ServiceResult<ArtistResponse>>(`/artists/${id}`);
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+
+  return response.data.data!;
+};
+
+export const getArtistWithEvents = async (id: number): Promise<ArtistWithEventsResponse> => {
+  const response = await api.get<ServiceResult<ArtistWithEventsResponse>>(`/artists/${id}/events`);
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+
+  return response.data.data!;
+};
+
+export const createArtist = async (request: CreateArtistRequest): Promise<ArtistResponse> => {
+  const response = await api.post<ServiceResult<ArtistResponse>>("/artists", request);
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+
+  return response.data.data!;
+};
+
+export const updateArtist = async (request: UpdateArtistRequest): Promise<void> => {
+  const response = await api.put<ServiceResult<null>>(`/artists/${request.id}`, request);
+
+  if (response.status === 204) return;
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+};
+
+export const passiveArtist = async (id: number): Promise<void> => {
+  const response = await api.patch<ServiceResult<null>>(`/artists/${id}/passive`);
+
+  if (response.status === 204) return;
+
+  if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+};
+
+// export const searchArtists = async (name: string): Promise<ArtistResponse[]> => {
+//     const response = await api.get<ServiceResult<ArtistResponse[]>>('/artists/search', {
+//         params: { name }
+//     });
+//     if (!response.data.isSuccess) throw new Error(response.data.errorMessages.join(", "));
+//     return response.data.data!;
+// };
